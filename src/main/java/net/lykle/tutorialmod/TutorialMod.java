c40532db953e031fa84b102abb4ad97d7f3519cf
Gabriel <gabrielriveropersonal@gmail.com>
2024-12-31 10:42:44 -0500

    testing comment

--- src/main/java/net/lykle/tutorialmod/TutorialMod.java
+++ src/main/java/net/lykle/tutorialmod/TutorialMod.java
@@ -5,8 +5,9 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// testing comment
 public class TutorialMod implements ModInitializer {
-	
+
 	public static final String MOD_ID = "tutorialmod";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
